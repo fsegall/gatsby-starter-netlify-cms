f48f830284cf8c440aeed8f345b8a506dc7e6207
fsegall <segall.felipe@gmail.com>
2021-01-15 02:16:15 -0300

    Fixes navbar

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -70,7 +70,7 @@ export const IndexPageTemplate = ({
           <div className="columns">
             <div className="column is-10 is-offset-1">
               <div className="content">
-                <div>
+                <div className="content">
                   <div className="tile">
                     <h1 className="title">{mainpitch.title}</h1>
                   </div>
